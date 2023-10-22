dd73918b1182948308c519ab0f4fd06664be4a67
BraysHub <120530815+BraysHub@users.noreply.github.com>
2023-10-22 16:13:50 -0400

    Create credits.c

---
+++ src /credits.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#ifndef BAKERAIN_DEVELOPE_R
+#define BAKERAIN_DEVELOPE_R "kok3shidoll"
+#endif
+#define THE_PLUSH "Ploosh"
+
+#define T void
+#define R print_credits(void) {
+#define O fprintf
+#define U (
+#define B stderr
+#define L ,
+#define E "# == AppEscape == \n#\n"
+#define M "# Made by: BraysBay, " THE_PLUSH ", MyRealNameIsC00kie, Nebula, M12l, " BAKERAIN_DEVELOPE_R " \n#\n"
+#define A "# Thanks to: pythonplayer123, llsc12, Mineek, tihmstar, nikias\n"
+#define K "# (libimobiledevice), checkra1n team (Siguza, axi0mx, littlelailo\n"
+#define I "# et al.), Procursus Team (Hayden Seay, Cameron Katri, Keto et.al)\n\n"
+#define N )
+#define G ;
+#define S }
+
+T R O U B L E M A K I N G S
